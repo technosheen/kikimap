773eec5e307f20d487777804aae692dd3a2e1b5f
technosheen <technosheen@icloud.com>
2023-12-29 20:00:19 -0500

    Initial Commit

---
+++ kikimap/kikimapApp.swift
@@ -0,0 +1,32 @@
+//
+//  kikimapApp.swift
+//  kikimap
+//
+//  Created by Sean Mahoney on 12/29/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct kikimapApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
